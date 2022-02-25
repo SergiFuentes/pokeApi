2e98dc5b331cf67340832a04f5b8db8d28e252f6
SergiFuentes <you@example.com>
2022-02-25 10:58:07 +0100

    adding detailed card

--- src/components/DetailedCard/DetailedCard.jsx
+++ src/components/DetailedCard/DetailedCard.jsx
@@ -1,34 +1,36 @@
-/* import axios from "axios"; */
 import { useEffect, useState } from "react";
 import { getPokemon } from "../../service/apiService";
-import './Card.css';
+import './DetailedCard.css';
 
-export function Card(props) {
+export function DetailedCard(props) {
   const [pokemonImg, setPokemonImg] = useState("");
   const [pokemonName, setPokemonName] = useState("");
+  const [pokemonHeight, setPokemonHeight] = useState("");
+  const [pokemonWeight, setPokemonWeight] = useState("");
 
   useEffect(() => {
     getPokemon(props.pokemon.url).then((pokeData) => {
       setPokemonImg(pokeData.sprites.other.dream_world.front_default);
-      /* axios
-      .get(props.pokemon.url)
-      .then((res) =>
-        setPokemonImg(res.data.sprites.other.dream_world.front_default)
-      ); */
       setPokemonName( pokeData.name );
+      setPokemonHeight( pokeData.height );
+      setPokemonWeight( pokeData.weight );
       console.log( pokeData );
     });
   }, [props]);
 
   return (
-    <div className="card">
-      <div className="cardImage">
+    <div className="detailedCard">
+      <div className="detailedCardImage">
         <img className="pokemonImage" src={pokemonImg} alt={pokemonName} />
       </div>
-      <div className="cardName">
+      <div className="detailedCardName">
         <button id="viewButton">VIEW</button>
         <p className="pokemonNameTitle">Pokemon name: </p>
         <h1 className="pokemonName">{pokemonName}</h1>
+        <p className="pokemonHeightTitle">Height: </p>
+        <h1 className="pokemonHeight">{pokemonHeight}</h1>
+        <p className="pokemonNameWeight">Wheight: </p>
+        <h1 className="pokemonWeight">{pokemonWeight}</h1>
       </div>
     </div>
   );
